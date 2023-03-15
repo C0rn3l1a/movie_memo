a696f4fd75c0d1857bddf5bcbf7f6f74d16a545d
Nacho <nachoborello97@gmail.com>
2023-03-15 22:54:19 +1100

    tweaks

--- crates/db/src/schemas/user.rs
+++ crates/db/src/schemas/user.rs
@@ -54,15 +54,15 @@ impl User {
             return Err(Report::new(UserError::AlreadyExists(message.clone())).attach_printable(message.clone()));
         }
 
-        let users = sqlx::query_as!(User,"INSERT INTO public.user (id, username, created_on, updated_on) VALUES (gen_random_uuid(), $1, NOW(), NOW()) RETURNING *;", username)
-            .fetch_all(&pool)
+        let user = sqlx::query_as!(User,"INSERT INTO public.user (id, username, created_on, updated_on) VALUES (gen_random_uuid(), $1, NOW(), NOW()) RETURNING *;", username)
+            .fetch_one(&pool)
             .await
-            .map_err(|_| {
+            .map_err(|error| {
                 let message = String::from("User creation failed with params: ({username})");
-                Report::new(UserError::InvalidArguments(message.clone())).attach_printable(message.clone())
+                Report::new(UserError::InvalidArguments(message.clone())).attach_printable(message.clone()).attach(error)
             })?;
 
-        Ok(users.into_iter().next().unwrap())
+        Ok(user)
     }
     
     pub async fn exists(username: &str) -> Result<bool, UserError>{
@@ -78,7 +78,7 @@ impl User {
             Ok(users) => Ok(users.len() > 0),
             Err(error) => {
                 let message = format!("Sqlx Error on [ User::exists ] : {error}");
-                Err(Report::new(UserError::SqlxError).attach_printable(message))
+                Err(Report::new(UserError::SqlxError).attach_printable(message).attach(error))
             }
         }
     }
