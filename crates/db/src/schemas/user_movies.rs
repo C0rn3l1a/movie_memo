52762667a48f4fef1dce440e9ff4d404d574ca62
Ignacio Borello <ignacio@icanotes.com>
2023-03-26 19:37:15 +1100

    Added create user movie endpoint

--- crates/db/src/schemas/user_movies.rs
+++ crates/db/src/schemas/user_movies.rs
@@ -42,7 +42,60 @@ pub struct UserMovie {
     updated_on: NaiveDateTime,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+pub struct NewMovieParams {
+    pub movie_id: i32,
+    pub title: String,
+    pub user_id: Option<Uuid>,
+    pub seen: Option<bool>,
+    pub watch_again: Option<bool>,
+    pub rating: Option<i32>,
+}
+
 impl UserMovie {
+    pub async fn new(params: NewMovieParams) -> Result<UserMovie, UserMovieError> {
+        let movie_id = params.movie_id;
+        let title = params.title;
+        let user_id = match params.user_id {
+            Some(user_id) => user_id,
+            None => return Err(Report::new(UserMovieError::InvalidArguments(String::from("user_id is required"))))
+        };
+        let seen = params.seen.unwrap_or(false);
+        let watch_again = params.watch_again.unwrap_or(false);
+        let rating = params.rating;
+
+        let pool = get_connection_pool().await.map_err(|error| {
+            let message = format!("Sqlx Error on [ get_connection_pool ] : {error}");
+            error.change_context(UserMovieError::SqlxError).attach_printable(message)
+        })?;
+        
+        let user_movie_exists = UserMovie::exists(user_id, movie_id).await.map_err(|error| {
+            let message = format!("Sqlx Error on [ User::exists ] : {error}");
+            error.change_context(UserMovieError::SqlxError).attach_printable(message)
+        })?;
+
+        if user_movie_exists {
+            let message = String::from("UserMovie already exists");
+            return Err(Report::new(UserMovieError::AlreadyExists(message.clone())).attach_printable(message.clone()));
+        }
+
+        let user_movie = sqlx::query_as!(UserMovie,
+            "
+            INSERT INTO public.user_movie 
+            (movie_id, title, user_id, seen, watch_again, rating, created_on, updated_on) 
+            VALUES 
+            ($1, $2, $3, $4, $5, $6, NOW(), NOW()) RETURNING *;
+            ", movie_id,title,user_id,seen,watch_again,rating)
+            .fetch_one(&pool)
+            .await
+            .map_err(|error| {
+                let message = format!("UserMovie creation failed with params: ({},{:?},{},{},{},{:?})", movie_id,title,user_id,seen,watch_again,rating);
+                Report::new(UserMovieError::InvalidArguments(message.clone())).attach_printable(message.clone()).attach(error)
+            })?;
+
+        Ok(user_movie)
+    }
+
     pub async fn get_by_user_id(user_id: &Uuid) -> Result<Vec<UserMovie>, UserMovieError>{
         let pool = get_connection_pool().await.map_err(|error| {
             let message = format!("Sqlx Error on [ get_connection_pool ] : {error}");
@@ -58,5 +111,23 @@ impl UserMovie {
             })?;
 
         Ok(user_movies)
+    }
+
+    pub async fn exists(user_id: Uuid, movie_id: i32) -> Result<bool, UserMovieError>{
+        let pool = get_connection_pool().await.map_err(|error| {
+            let message = format!("Sqlx Error on [ get_connection_pool ] : {error}");
+            error.change_context(UserMovieError::SqlxError).attach_printable(message)
+        })?;
+        
+        let movies = sqlx::query!("SELECT um.id FROM public.user_movie um where um.user_id = $1 AND um.movie_id = $2;", user_id, movie_id)
+            .fetch_all(&pool).await;
+
+        match movies {
+            Ok(movies) => Ok(movies.len() > 0),
+            Err(error) => {
+                let message = format!("Sqlx Error on [ UserMovie::exists ] : {error}");
+                Err(Report::new(UserMovieError::SqlxError).attach_printable(message).attach(error))
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::env;
 use dotenv::dotenv;
 use actix_web::{get, web, App, HttpResponse, HttpServer, post};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use movie_memo_db::schemas::movie;
 use movie_memo_db::schemas::user::{User, UserError};
-use movie_memo_db::schemas::user_movies::{UserMovie, UserMovieError};
+use movie_memo_db::schemas::user_movies::{UserMovie, UserMovieError, NewMovieParams};
 use sqlx::types::Uuid;
 
 
@@ -54,6 +54,49 @@ async fn fetch_url2(params: web::Path<GetUserMoviesParams>) -> HttpResponse {
 }
 
 #[derive(Deserialize)]
+struct CreateUserMoviesParams {
+    user_id: Uuid,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct CreateUserMovieBody {
+    movie_id: i32,
+    title: String,
+    seen: Option<bool>,
+    watch_again: Option<bool>,
+    rating: Option<i32>,
+}
+
+#[post("/user/{user_id}/movies")]
+async fn create_user_movie(params: web::Path<CreateUserMoviesParams>, body: web::Json<NewMovieParams>) -> HttpResponse {
+    
+    println!("[GET] /user/{}/movies", params.user_id);
+
+    let params = NewMovieParams {
+        user_id: Some(params.user_id),
+        movie_id: body.movie_id,
+        title: body.title.clone(),
+        rating: body.rating,
+        seen: body.seen,
+        watch_again: body.watch_again
+    };
+
+    match UserMovie::new(params).await {
+        Ok(movies) => HttpResponse::Ok().json(movies),
+        Err(err) => {
+            let context = err.current_context().clone();
+
+            match context {
+                UserMovieError::AlreadyExists(msg) => HttpResponse::BadRequest().body(format!("{}", msg)),
+                UserMovieError::InvalidArguments(msg) => HttpResponse::BadRequest().body(format!("{}", msg)),
+                UserMovieError::NotFound(msg) => HttpResponse::NotFound().body(format!("{}", msg)),
+                UserMovieError::SqlxError => HttpResponse::InternalServerError().body("Something went wrong. Try Again later")
+            }
+        },
+    }
+}
+
+#[derive(Deserialize)]
 struct CreateUserRequest {
     username: String,
 }
@@ -81,16 +124,30 @@ async fn create_user(req_body: web::Json<CreateUserRequest>) -> HttpResponse {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok(); // This line loads the environment variables from the ".env" file.
+    check_environment();
     let port = env::var("PORT").unwrap_or(String::from("8000"));
     let port_number = port.parse::<u16>().unwrap_or(8000);
-    println!("Server running on port {}",port);
+    println!("Server running on port \x1b[32m{}\x1b[0m",port);
     HttpServer::new(|| {
         App::new()
             .service(fetch_url)
             .service(fetch_url2)
             .service(create_user)
+            .service(create_user_movie)
     })
     .bind(("127.0.0.1", port_number))?
     .run()
     .await
+}
+
+fn check_environment() {
+    if let Err(_) = env::var("TMDB_API_KEY_V3") {
+        panic!("TMDB_API_KEY_V3 must be set in order to consume TMBD API.");
+    }
+    if let Err(_) = env::var("TMDB_URL_V3") {
+        panic!("TMDB_URL_V3 must be set in order to consume TMBD API. (it's assumed the url does not finish with `/`)");
+    }
+    if let Err(_) = env::var("DATABASE_URL") {
+        panic!("DATABASE_URL must be set in order to perform database operations. (format `postgres://{{DB_USER}}:{{DB_PASSWORD}}@{{DB_HOST}}/movie_memo`)");
+    }
 }
